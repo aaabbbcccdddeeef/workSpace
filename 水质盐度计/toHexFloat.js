7b812871008724bc823c872fdce8dcdf745c7b5c
xuqingfeng <1063929753@qq.com>
2022-08-09 14:41:42 +0800

    十进制浮点数转换十六进制浮点数(js)

--- 水质盐度计/toHexFloat.js
+++ 水质盐度计/toHexFloat.js
@@ -3,68 +3,66 @@ function toHexFloat(data) {
     var header //整数部分
     var footer//小数部分
     header = dataArr[0] !== '0' ? dataArr[0] : 0
-    footer = dataArr[1] !== '0' ? dataArr[1]:0
-    if (header=='0'&&footer=='0'){
+    footer = dataArr[1] !== '0' ? dataArr[1] : 0
+    if (header == '0' && footer == '0') {
         return '00000000'
     }
-    var SS=parseFloat(data)>=0?0:1//S,符号位
-    header=Math.abs(header)
-    var arr1=[]//二进制整数
-    var arr2=[] //二进制小数
+    var SS = parseFloat(data) >= 0 ? 0 : 1//S,符号位
+    header = Math.abs(header)
+    var arr1 = []//二进制整数
+    var arr2 = [] //二进制小数
     if (header !== 0) {
-        let i=0,a
+        let i = 0, a
         header = parseInt(header)
         while (true) {
             a = header % 2
-            arr1[i]=a
+            arr1[i] = a
             i++
-            header=Math.floor(header/2)
+            header = Math.floor(header / 2)
             if (header == 0) {
                 break;
             }
         }
-    }
-    else {
-        arr1[0]=header
+    } else {
+        arr1[0] = header
     }
     if (footer !== 0) {
-        let a,b,j=0
+        let a, b, j = 0
         while (true) {
-            footer='0.'+footer
-            a = (footer*2).toString()
-            b=a.split('.')
-            arr2[j]=b[0]
-            footer=b[1]
+            footer = '0.' + footer
+            a = (footer * 2).toString()
+            b = a.split('.')
+            arr2[j] = b[0]
+            footer = b[1]
             j++
-            if (j>=10) {
+            if (j >= 10) {
                 break;
             }
         }
+    } else {
+        arr2[0] = 0
     }
-    else {
-        arr2[0]=0
-    }
-    arr1=arr1.reverse() //整数逆取，小数顺取
-    var arr1str='',arr2str=''
-    arr1.forEach(value=>arr1str+=value)
-    arr2.forEach(value=>arr2str+=value)
+    arr1 = arr1.reverse() //整数逆取，小数顺取
+    var arr1str = '', arr2str = ''
+    arr1.forEach(value => arr1str += value)
+    arr2.forEach(value => arr2str += value)
     var obstr
-    if (arr1str.length>1) {
+    if (arr1str.length > 1) {
         obstr = arr1str.substr(0, 1) + '.' + arr1str.substr(1, arr1str.length) + arr2str
-    }
-    else {
+    } else {
         obstr = arr1str.substr(0, 1) + '.' + arr2str
     }
     console.log(obstr)
-    var EE=(arr1str.length-1)+127 //E
-    EE=EE.toString(2).padStart(8,'0') //8位
-    var MM=(obstr.substr(2,obstr.length)).padEnd(23,'0')//M
-    var bin=SS+EE+MM
-    var endHex=parseInt(bin,2).toString(16)
-    console.log(EE,MM,bin,endHex)
+    var EE = (arr1str.length - 1) + 127 //E
+    EE = EE.toString(2).padStart(8, '0') //8位
+    var MM = (obstr.substr(2, obstr.length)).padEnd(23, '0')//M
+    var bin = SS + EE + MM
+    var endHex = parseInt(bin, 2).toString(16)
+    console.log(EE, MM, bin, endHex)
     return endHex
 }
-var oo=toHexFloat('1.0')
+
+var oo = toHexFloat('1.0')
 console.log(oo)
 // console.log(parseFloat('2.123')>0?0:1)
 
