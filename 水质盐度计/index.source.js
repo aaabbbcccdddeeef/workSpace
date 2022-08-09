9f857a5e7bbfe5354b2f3fb54e7ebf325dc7e2f5
xuqingfeng <1063929753@qq.com>
2022-08-09 16:21:34 +0800

    水质盐值探测仪rs485

--- 水质盐度计/index.source.js
+++ 水质盐度计/index.source.js
@@ -188,19 +188,82 @@ Relay.prototype.read = function (addr, code) {
         }
     }
 }
+
+function toHexFloat(data) {
+    var dataArr = data.split('.')
+    var header //整数部分
+    var footer//小数部分
+    header = dataArr[0] !== '0' ? dataArr[0] : 0
+    footer = dataArr[1] !== '0' ? dataArr[1] : 0
+    if (header == '0' && footer == '0') {
+        return '00000000'
+    }
+    var SS = parseFloat(data) >= 0 ? 0 : 1//S,符号位
+    header = Math.abs(header)
+    var arr1 = []//二进制整数
+    var arr2 = [] //二进制小数
+    if (header !== 0) {
+        let i = 0, a
+        header = parseInt(header)
+        while (true) {
+            a = header % 2
+            arr1[i] = a
+            i++
+            header = Math.floor(header / 2)
+            if (header == 0) {
+                break;
+            }
+        }
+    } else {
+        arr1[0] = header
+    }
+    if (footer !== 0) {
+        let a, b, j = 0
+        while (true) {
+            footer = '0.' + footer
+            a = (footer * 2).toString()
+            b = a.split('.')
+            arr2[j] = b[0]
+            footer = b[1]
+            j++
+            if (j >= 10) {
+                break;
+            }
+        }
+    } else {
+        arr2[0] = 0
+    }
+    arr1 = arr1.reverse() //整数逆取，小数顺取
+    var arr1str = '', arr2str = ''
+    arr1.forEach(value => arr1str += value)
+    arr2.forEach(value => arr2str += value)
+    var obstr
+    if (arr1str.length > 1) {
+        obstr = arr1str.substr(0, 1) + '.' + arr1str.substr(1, arr1str.length) + arr2str
+    } else {
+        obstr = arr1str.substr(0, 1) + '.' + arr2str
+    }
+    console.log(obstr)
+    var EE = (arr1str.length - 1) + 127 //E
+    EE = EE.toString(2).padStart(8, '0') //8位
+    var MM = (obstr.substr(2, obstr.length)).padEnd(23, '0')//M
+    var bin = SS + EE + MM
+    var endHex = parseInt(bin, 2).toString(16)
+    console.log(EE, MM, bin, endHex)
+    return endHex
+}
+
 // 写入
 //addr为设备地址编号,code为控制节点,state为行为,字符串
 Relay.prototype.write = function (addr, code, state) {
     if (typeof addr === 'number') addr = addr.toString(16)
+    if (typeof state === 'number') state = state.toString()
     while (addr.length < 2) {
         addr = '0' + addr;
     }
     var cmd = [];
-
-    state=toHex(state);
-    while (state.length<4){
-        state='0'+ state;
-    }
+    state = toHexFloat(state);
+    state=state.substr(4,4)+state.substr(0,4)
     if (code.indexOf("7") > -1) {//电导率常数设置
         var commond = this.validate.crc16(addr + '10000A000204' + state)
         cmd.push(commond)
